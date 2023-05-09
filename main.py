d6a4dd820582d6c9dbba02ac75f7dad65a7ed29e
NdayiragijeAlainFabrice <alainfabbricendayiragije98@gmail.com>
2023-05-09 20:34:44 +0200

    Display_data_q1

--- main.py
+++ main.py
@@ -3,3 +3,9 @@
 # creation de la liste :
 
 liste_elmnt = ['vache','mouton','chevre','canard','lapin','cochon','abeille','pagna','chat','poule']
+
+#----1------ : affichage des elements de la liste :
+
+print('1: voici la liste des elments de la liste:') 
+for element in liste_elmnt:
+    print (element)
