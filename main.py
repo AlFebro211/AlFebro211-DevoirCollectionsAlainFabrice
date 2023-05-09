7f9d9ae99d7bdd46d491576648230e04b49da028
NdayiragijeAlainFabrice <alainfabbricendayiragije98@gmail.com>
2023-05-09 20:38:30 +0200

    fill_2ndelist

--- main.py
+++ main.py
@@ -16,3 +16,16 @@ print("2: la nouvelle  liste apres le changement  de l'element numero 5:")
 liste_elmnt.insert (5,"grenouille")
 for element in liste_elmnt:
     print (element)
+    
+
+#----3------ : remplir la 2eme liste avec  les elements de la 1ere liste contenant la lettre 'a' :
+
+print ('3: voici les elements de la nouvelle liste :')
+
+
+liste_2 = []
+for elt in liste_elmnt:
+   if 'a' in elt:
+       liste_2.append(elt)
+for i in liste_2:
+    print(i)
