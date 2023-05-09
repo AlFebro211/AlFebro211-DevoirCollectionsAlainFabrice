963b93127159ce1459208e7cdfc2a804ee50ce70
NdayiragijeAlainFabrice <alainfabbricendayiragije98@gmail.com>
2023-05-09 20:36:42 +0200

    change_contentnumber5

--- main.py
+++ main.py
@@ -9,3 +9,10 @@ liste_elmnt = ['vache','mouton','chevre','canard','lapin','cochon','abeille','pa
 print('1: voici la liste des elments de la liste:') 
 for element in liste_elmnt:
     print (element)
+
+#----2------ : changer le contenu numero 5 :
+
+print("2: la nouvelle  liste apres le changement  de l'element numero 5:")
+liste_elmnt.insert (5,"grenouille")
+for element in liste_elmnt:
+    print (element)
