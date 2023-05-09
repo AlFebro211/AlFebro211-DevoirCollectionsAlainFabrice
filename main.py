5c9d191f8c3b4501d5642786cb83ca20ccaffe1d
NdayiragijeAlainFabrice <alainfabbricendayiragije98@gmail.com>
2023-05-09 20:27:32 +0200

    add main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+# ----------------QUESTION I----------------------
+
+# creation de la liste :
+
+liste_elmnt = ['vache','mouton','chevre','canard','lapin','cochon','abeille','pagna','chat','poule']
